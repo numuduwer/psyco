e7d9d120338677f8605a435bf8b60f237efd0559
youngsson2 <youngsson4@gmail.com>
2020-10-07 11:24:53 +0900

    [프로젝트 설정] JSONParser를 위한 json-simple 라이브러리 추가 및 네이버 로그인에 필요한 메서드들  추가, mybatis-config 파일 수정

--- src/com/three/psyco/service/bean/MemberService.java
+++ src/com/three/psyco/service/bean/MemberService.java
@@ -1,5 +1,17 @@
-package com.three.psyco.service.bean;
+package com.three.psyco.service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import org.json.simple.parser.ParseException;
 
 public interface MemberService {
 
+	public String naverLogin() throws UnsupportedEncodingException;
+	
+	public StringBuffer callBack() throws UnsupportedEncodingException;
+	
+	public HashMap<String, String> MemberProfile(StringBuffer res) throws ParseException;
+	
+	public int existUserCheck(HashMap<String, String> MemberProfile);
 }
