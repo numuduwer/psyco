ebb81aa47661f7761506f7b329638c19480e106b
numuduwer <33523029+numuduwer@users.noreply.github.com>
2020-10-12 17:54:49 +0900

    동윤이 브런치를 건드는 김신 1

--- src/com/three/psyco/controller/bean/MainBean.java
+++ src/com/three/psyco/controller/bean/MainBean.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import com.three.psyco.model.dto.TestDTO;
+
 
 
 @Controller
@@ -21,13 +21,7 @@ public class MainBean {
 	public String main() {
 		System.out.println("controller 잘 연결 ");
 		
-		
-		TestDTO dto = new TestDTO();
-		dto.setNum(99);
-		dto.setWriter("99");
-		
-		int count = sqlSession.selectOne("test.put",dto);
-		System.out.println("count :" + count );
+
 		return "main/main";
 	}
 
--- src/com/three/psyco/model/dao/SuperDAO.java
+++ src/com/three/psyco/model/dao/SuperDAO.java
@@ -5,10 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface SuperDAO {
-	
-	
-	
-	
 	public int count(String pageName)throws SQLException;
 	public List List(String pageName, int startRow, int endRow) throws SQLException;
 	public int delete(String pageName, String id) throws SQLException;
