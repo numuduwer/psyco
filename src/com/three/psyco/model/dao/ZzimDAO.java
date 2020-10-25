519bcea7f8bfe1d92f1c89fef2e785003eedd56e
numuduwer <33523029+numuduwer@users.noreply.github.com>
2020-10-25 12:56:57 +0900

    dd

--- src/com/three/psyco/model/dao/ZzimDAO.java
+++
@@ -1,5 +0,0 @@
-package com.three.psyco.model.dao;
-
-public interface ZzimDAO {
-
-}
--- src/com/three/psyco/model/dto/BigDTO.java
+++
@@ -1,7 +0,0 @@
-package com.three.psyco.model.dto;
-
-public class BigDTO {
-	
-	private int big_num;
-	private String big_category;
-}
